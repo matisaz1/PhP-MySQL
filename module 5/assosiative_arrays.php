951e9a1d934d95d6a8742e9ff8539755ed76c68c
matisaz1 <matthewyessir1122@gmail.com>
2026-03-04 20:45:28 +0100

    Module 5

---
+++ module 5/assosiative_arrays.php
@@ -0,0 +1,25 @@
+<?php
+
+$grade = array(
+    "Math" => "4",
+    "Art" => "5",
+    "History" => "4",
+    "Music" => "4",
+)
+
+// $grade = ['Math'] = "4",
+// $grade = ['Art'] = "5",
+// $grade = ['History'] = "4",
+// $grade = ['Music'] = "4",
+
+ echo "Math grade is".$grade['Math'];
+ echo "Art grade is".$grade['Art'];
+ echo "History grade is".$grade['History'];
+ echo "Music grade is".$grade['Music'];
+
+ foreach ($grade as $subject => $grade){
+    echo "Subject: ".$subject." Grade : ".$grade;
+    echo "<br>";
+ }
+
+?>
---
+++ module 5/multidimensional_arrays.php
@@ -0,0 +1,18 @@
+$dogs = array(
+    array ("Chihuahua", "Mexico", "20"),
+    array ("Husky", "Siberia", "15"),
+    array ("Bulldog", "England", "10"),
+);
+
+echo $dogs[0][0], ": Origin:".$dogs[0][1]. ", Life Span: ".$dogs [0][2]. ".<br>";
+echo $dogs[1][0], ": Origin:".$dogs[1][1]. ", Life Span: ".$dogs [1][2]. ".<br>";
+echo $dogs[2][0], ": Origin:".$dogs[2][1]. ", Life Span: ".$dogs [2][2]. ".<br>";
+
+for ($row = 0; $row < 3; $row++){
+    echo "<p><b>Row number $row </b></p>";
+    echo "<ul>";
+    for ($col = 0; $col < 3; $col++){
+        echo "<li>".$dogs [$row][$col]."</li>";
+    }
+    echo "</ul>"
+}
